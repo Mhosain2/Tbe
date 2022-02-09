7e21f71ef7a776a1cb5cec7b81c80dd7a677d209
g2902 <mhossain646917@yahoo.com>
2022-02-08 20:22:43 -0500

    This is my second commit.

---
+++ ThisIs/src/Trouble.java
@@ -0,0 +1,16 @@
+
+public class Trouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 45;
+		int c = 54;
+		int d = a+c;
+		String greeting ="this is my world and i will live in it";
+		
+		System.out.println(d);
+		System.out.println(greeting);
+		 
+	}
+
+}
